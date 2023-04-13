7dc5d5f26bfe30d7b5e40e974a5b59884b194d57
Ubuntu <ubuntu@ip-172-31-16-196.ec2.internal>
2023-04-13 12:17:04 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is to demostatre mlops using mlflow for deep learning',
+    author='obulesu',
+    license='MIT',
+)
